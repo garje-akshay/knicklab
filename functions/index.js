7fb4a73e2714ae6a76b2cea8d30fc18a34636e64
Akshay Garje <32763618+knicklab@users.noreply.github.com>
2022-02-12 12:53:53 +0530

    removed cloud function from firebase

--- functions/index.js
+++
@@ -1,5 +0,0 @@
-const functions = require("firebase-functions");
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!");
-  response.send("Hello from Firebase!");
-});
