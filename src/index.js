294c71224c94a4c1a8c54ef2dc0a35b5dd6dba8d
Akshay Garje <32763618+knicklab@users.noreply.github.com>
2022-02-12 14:10:44 +0530

    removed unwanted

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from "./reportWebVitals";
-import app from "./firebase";
+import "./firebase";
 
 ReactDOM.render(
   <React.StrictMode>
