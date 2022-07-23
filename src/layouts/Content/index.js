44445fa732d3c5128f3eb4125bc51a6e263c1cf4
Akshay Garje <akshay.garje@outlook.com>
2022-07-24 03:08:45 +0530

    chnage in layout

--- src/layouts/Content/index.js
+++ src/layouts/Content/index.js
@@ -7,24 +7,35 @@ function Content() {
       <h1
         style={{
           fontFamily: "'Edu NSW ACT Foundation', cursive",
-          margin: 10,
+          marginTop: 10,
+          margin: 0,
         }}
       >
         KnickLab
       </h1>
-      <h4>by Akshay Garje.</h4>
-      <h3> About me </h3>
+      <h4 style={{ margin: 0 }}>by Akshay Garje.</h4>
+      <h3 style={{ margin: "50px 0 0 5px" }}> About me. </h3>
       <p
         style={{
           maxWidth: "50vw",
+          marginBottom: "20px",
         }}
       >
-        Hi I am Akshay Garje, a 25 year old Web developer living in Pune, India.
-        I am an IT Engineer, currently working with awesome folks at GS Lab.
-        Have a look at skills or just connect with me on LinkedIn. I am always
-        excited to do business with like minded people, lets discuss over
-        coffee.
+        Hi, I am Akshay Garje, a {new Date().getFullYear() - 1997} years old Web
+        developer living in{" "}
+        <a href="https://goo.gl/maps/U6YWoFzZ9SP4MWNSA">Pune, India.</a> I am an
+        IT Engineer, currently working with awesome folks at{" "}
+        <a href="https://www.gslab.com/">GS Lab</a>. Have a look at skills or
+        just connect with me on LinkedIn. I am always excited to do business
+        with like minded people, lets discuss over coffee. Cheers!!
       </p>
+      <span>
+        <a
+          href="https://www.linkedin.com/in/akshaygarje/"
+          class="fa fa-linkedin"
+        ></a>
+        <a href="mailto:akshay.garje@outlook.com" class="fa fa-envelope"></a>
+      </span>
     </div>
   );
 }
