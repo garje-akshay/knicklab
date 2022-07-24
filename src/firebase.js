2a6c313749997d2edcdc4b4a984b7c4750204ed5
Akshay Garje <akshay.garje@outlook.com>
2022-07-24 14:42:56 +0530

    comented unwanted code

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getDatabase } from "firebase/database";
+//import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+//import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+/*const firebaseConfig = {
   apiKey: "AIzaSyBkKPe6plgGjDkhZET_PJd3a6sNIRR0MrA",
   authDomain: "knicklab-e7859.firebaseapp.com",
   projectId: "knicklab-e7859",
@@ -20,4 +20,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const database = getDatabase(app);
+const database = getDatabase(app);*/
